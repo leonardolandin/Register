4e9fe1d7fc83af7c06e3d2716103ad8b1257ef70
Ajudando <ajudando.ajudinha07@gmail.com>
2019-11-05 23:04:16 -0300

    initial

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./style/main.css">
+    <title>Falcon Heavy</title>
+</head>
+<?php
+require_once "./utilsPHP/database.php"
+?>
+<body>
+    <div class="background">
+        <div class="container-form">
+            <form action="" class="form-login">
+                <a href=""><img src="">LOGO DA EMP</a>
+                <h1>Login</h1>
+                <div>
+                    <label for="">E-mail:</label>
+                    <input type="text" placeholder="Digite seu e-mail" style="padding-left: 10px;">
+                </div>
+                <div>
+                    <label for="">Senha:</label>
+                    <input type="password" placeholder="Digite sua senha" style="padding-left: 10px;">
+                </div>
+                <div style="display: flex; flex-direction: row-reverse;">
+                    <input type="submit" name="login" id="Entrar" class="login">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ utilsPHP/database.php
@@ -0,0 +1,13 @@
+<?php
+$server = "remotemysql.com";
+$user = "OSjJLwSUBg";
+$password = "LYvPpebZrS";
+$db = "OSjJLwSUBg";
+
+
+
+$connect = mysqli_connect($server, $user, $password, $db);
+if(!$connect) {
+    echo "deu ruim";
+}
+?>
