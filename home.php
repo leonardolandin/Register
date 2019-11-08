190f0e11fbeb9e9ab91007d193202092e72cda98
Ajudando <ajudando.ajudinha07@gmail.com>
2019-11-07 22:17:39 -0300

    decision

--- home.php
+++ home.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./style/main.css">
+    <link rel="stylesheet" type="text/css" href="./style/home.css">
     <title>Falcon Heavy - Admin</title>
 </head>
 <body>
-    
+    <div class="background decision">
+        <a href="./company.php">
+            <div class="decision-container">
+                <img src="./img/company.png">
+                <span>Cadastrar Empresa</span>
+            </div>
+        </a>
+        <a href="./index.php">
+            <div class="decision-container">
+                <img src="./img/leave.png">
+                <span>Sair</span>
+            </div>
+        </a>
+    </div>
 </body>
 </html>
--- utilsPHP/login.php
+++ utilsPHP/login.php
@@ -2,18 +2,20 @@
 require_once "./utilsPHP/database.php";
 session_start();
 
-if(isset($_POST['email']) and isset($_POST['password'])) {
+if($_POST['login']) {
     $email = $_POST['email'];
     $senha = $_POST['password'];
-    $query = mysqli_query($connect, "SELECT * FROM login WHERE email = '{$email}' and admin = '1'");
+    if(!empty($email) and !empty($senha)) {
+        $query = mysqli_query($connect, "SELECT * FROM login WHERE email = '{$email}' and admin = '1'");
 
-    if($query) {
-        while($result = mysqli_fetch_array($query)) {
-            if($result["email"] == $email and $result["senha"] == $senha) {
-                $_SESSION['email'] = $email;
-                $_SESSION['senha'] = $senha;
-                mysqli_close($connect);
-                header("Location: ../home.php");
+        if($query) {
+            while($result = mysqli_fetch_array($query)) {
+                if($result["email"] == $email and $result["senha"] == $senha) {
+                    $_SESSION['email'] = $email;
+                    $_SESSION['senha'] = $senha;
+                    mysqli_close($connect);
+                    header("Location: ../home.php");
+                }
             }
         }
     }
