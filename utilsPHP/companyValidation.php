ce43ed1a80437d4f0139a7342014b41c8178f90b
Ajudando <ajudando.ajudinha07@gmail.com>
2019-11-10 15:28:00 -0300

    finnaly

--- utilsPHP/companyValidation.php
+++ utilsPHP/companyValidation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "./utilsPHP/company.php";
+require_once "./utilsPHP/database.php";
 
 if($_POST['login']) {
     $name = $_POST['companyName'];
@@ -17,11 +17,28 @@ if($_POST['login']) {
     
     if(!empty($name) and !empty($cnpj) and !empty($street) and !empty($number) and !empty($complement) and !empty($cep) 
     and !empty($county) and !empty($uf) and !empty($tel) and !empty($userEmail) and !empty($userPassword)) {
-        
-        $company = new Company;
-        $register = $company -> companyRegister($name, $cnpj, $street, $number, $complement, $cep, $county, $uf, $tel);
-        if($register == true) {
-            echo "<div class='success-validation'><span>Empresa cadastrada com sucesso!!</span></div>";
+        $read = mysqli_query($connect, "SELECT * FROM empresas WHERE nome_empresarial = '$name' and cnpj = '$cnpj' and cep = '$cep'");
+        if(mysqli_num_rows($read) == false) {
+            $query = mysqli_query($connect, "INSERT INTO empresas (nome_empresarial, cnpj, lougradouro, numero, complemento, cep, monicipio, uf, telefone) 
+            VALUES ('$name', '$cnpj', '$street', '$number', '$complement', '$cep', '$county', '$uf', '$tel')");
+
+            if($query == true) {
+                $idCompany = mysqli_query($connect, "SELECT * FROM empresas WHERE nome_empresarial = '$name' and cnpj = '$cnpj' and '$cep'");
+                if($idCompany) {
+                    while($result = mysqli_fetch_array($idCompany)) {
+                        $id = $result["id_empresa"];
+                    }
+                }
+
+                $queryPessoa = mysqli_query($connect, "INSERT INTO login (email, senha, empresa) VALUES ('$userEmail', '$userPassword', '$id')");
+
+                if ($queryPessoa == true) {
+                    echo "<div class='success-validation'><span>Empresa cadastrada com sucesso!!</span></div>";
+                    mysqli_close($connect);
+                }
+            }
+        } else {
+            "<div class='error-validation'><span>Empresa já é cadastrada</span></div>";
         }
     } else {
         echo "<div class='error-validation'><span>Preencha todos os campos!!</span></div>";
