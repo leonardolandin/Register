450a2ef08710ed345a2ece9765303fb5d6b0f12b
Ajudando <ajudando.ajudinha07@gmail.com>
2019-11-09 21:07:06 -0300

    validações e inserção no sql

--- companyRegister.php
+++ companyRegister.php
@@ -8,11 +8,11 @@
     <link rel="stylesheet" type="text/css" href="./style/company.css">
     <title>Cadastrar Empresa</title>
 </head>
-<?php
-require_once "./utilsPHP/companyValidation.php";
-?>
 <body>
     <div class="company-container">
+        <?php
+            require_once "./utilsPHP/companyValidation.php";
+        ?>
         <div class="signup">
             <div class="container-title">
                 <span>Cadastrar Nova Empresa</span>
@@ -21,47 +21,56 @@ require_once "./utilsPHP/companyValidation.php";
                 <div>
                     <div>
                         <label for="">Nome da Empresa: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="companyName" id="" placeholder="Digite o nome da empresa" maxlength="100">
                     </div>
                     <div>
                         <label for="">CNPJ: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="cnpj" id="" placeholder="Digite o CNPJ da empresa" maxlength="20">
                     </div>
                 </div>
                 <div>
                     <div>
                         <label for="">Logradouro: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="rua" id="" placeholder="Digite o logradouro da empresa" maxlength="150">
                     </div>
                     <div>
                         <label for="">Numero: </label>
-                        <input type="text" name="" id="">
+                        <input type="tel" name="numero" id="" placeholder="Digite o numero da empresa" maxlength="10">
                     </div>
                 </div>
                 <div>
                     <div>
                         <label for="">Complemento: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="complemento" id="" placeholder="Digite o complemento da empresa" maxlength="50">
                     </div>
                     <div>
                         <label for="">CEP: </label>
-                        <input type="text" name="" id="">
+                        <input type="tel" name="cep" id="" placeholder="Digite o CEP da empresa" maxlength="10">
                     </div>
                 </div>
                 <div>
                     <div>
                         <label for="">Municipio: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="muni" id="" placeholder="Digite o municipio da empresa" maxlength="100">
                     </div>
                     <div>
                         <label for="">UF: </label>
-                        <input type="text" name="" id="">
+                        <input type="text" name="uf" id="" placeholder="Digite o UF da empresa" maxlength="2">
                     </div>
                 </div>
                 <div>
                     <div>
                         <label for="">Telefone: </label>
-                        <input type="text" name="" id="">
+                        <input type="tel" name="tel" id="" placeholder="Digite o telefone da empresa" maxlength="80">
+                    </div>
+                </div>
+                <div>
+                    <div style="margin-top: 40px;">
+                        <span style="margin-bottom: 10px; font-size: 25px; font-weight: bold;">Representante</span>
+                        <label for="">E-mail: </label>
+                        <input type="email" name="emailRepresentante" id="" style="margin-bottom: 10px;" placeholder="Digite o email do representante" maxlength="130">
+                        <label for="">Senha: </label>
+                        <input type="password" name="senhaRepresentante" id="" placeholder="Digite a senha do representante" maxlength="80">
                     </div>
                 </div>
                 <div class="cadastrar">
--- utilsPHP/companyValidation.php
+++ utilsPHP/companyValidation.php
@@ -1,4 +1,30 @@
 <?php
 session_start();
+require_once "./utilsPHP/company.php";
 
+if($_POST['login']) {
+    $name = $_POST['companyName'];
+    $cnpj = $_POST['cnpj'];
+    $street = $_POST['rua'];
+    $number = $_POST['numero'];
+    $complement = $_POST['complemento'];
+    $cep = $_POST['cep'];
+    $county = $_POST['muni'];
+    $uf = $_POST['uf'];
+    $tel = $_POST['tel'];
+    $userEmail = $_POST['emailRepresentante'];
+    $userPassword = $_POST['senhaRepresentante'];
+    
+    if(!empty($name) and !empty($cnpj) and !empty($street) and !empty($number) and !empty($complement) and !empty($cep) 
+    and !empty($county) and !empty($uf) and !empty($tel) and !empty($userEmail) and !empty($userPassword)) {
+        
+        $company = new Company;
+        $register = $company -> companyRegister($name, $cnpj, $street, $number, $complement, $cep, $county, $uf, $tel);
+        if($register == true) {
+            echo "<div class='success-validation'><span>Empresa cadastrada com sucesso!!</span></div>";
+        }
+    } else {
+        echo "<div class='error-validation'><span>Preencha todos os campos!!</span></div>";
+    }
+}
 ?>
